407c4035f3aff13f715d37a9cd1f1a50255e0770
KajalPourjalil <rina.skyler@yahoo.com>
2023-10-09 22:35:27 +0300

    updates

---
+++ script.js
@@ -0,0 +1,23 @@
+function convertCtoF(celsius)
+{
+    let fahrenheit = celsius * 9 / 5 + 32;
+
+    // return fahrenheit;
+    console.log(fahrenheit);
+}
+
+convertCtoF(30);
+
+
+
+
+function reverseString(str) {
+    let reversedStr = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversedStr += str[i];
+    }
+    // return reversedStr;
+    console.log(reversedStr);
+}
+
+reverseString('nice');
