3394e8bdae46071a588bb52e2abf696914734a7d
KajalPourjalil <rina.skyler@yahoo.com>
2023-10-25 13:27:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -100,3 +100,10 @@ function truncateString(str, num) {
 }
 
 truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+function findElement(arr, func) {
+    let num = 0;
+    return arr.find(func);
+}
+
+findElement([1,2,3,4], num => num % 2 === 0);
